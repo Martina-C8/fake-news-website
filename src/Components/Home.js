98863283f6968896ba441801342c1f248a3f4fb9
Martina-C8 <casettamartina@outlook.it>
2025-02-21 19:55:42 +0100

    Modifiche stile e contenuto

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -23,13 +23,13 @@ const Home = () => {
             <h1 className="display-4 text-primary mb-3">
               Benvenuti nel sito anti fake news!
             </h1>
-            <p className="lead text-muted mb-4">
+            <p className="lead mb-4">
               Le fake news ambientali stanno danneggiando la nostra capacità di comprendere e affrontare problemi cruciali come il cambiamento climatico, la biodiversità e la sostenibilità. Qui troverai strumenti per distinguere i fatti dalla disinformazione.
             </p>
           </motion.div>
         </Col>
       </Row>
-      <Row className="justify-content-center mt-5">
+      <Row className="justify-content-center text-center mt-5">
         <Col md={8} lg={6}>
           <motion.div
             initial={{ opacity: 0, y: 50 }}
@@ -43,7 +43,50 @@ const Home = () => {
           </motion.div>
         </Col>
       </Row>
-      <Row className="justify-content-center mt-5">
+      <Row className="justify-content-center text-center mt-5">
+        <Col md={10} lg={8}>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 1.5 }}
+          >
+            <h2 className="text-secondary mb-3">Unisciti al cambiamento!</h2>
+            <p className="text-muted mb-4">
+              La lotta contro la disinformazione è una battaglia che possiamo vincere solo insieme. Condividi la conoscenza, verifica le notizie e diventa parte attiva della comunità che protegge il nostro pianeta dalle bugie. 
+            </p>
+            <Button
+              variant="success"
+              size="smß"
+              href="/community"
+              className="px-4 py-2 rounded-pill btn-hover">
+              Unisciti a noi
+            </Button>
+          </motion.div>
+        </Col>
+      </Row>
+        {/* Google Form */}
+        <Row className="justify-content-center text-center mt-5">
+        <Col md={10} lg={8}>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
+            <h2 className="text-primary mb-3">Lascia la tua opinione!</h2>
+            <p className="text-muted mb-4">
+              Compila il modulo qui sotto per condividere il tuo punto di vista sulle fake news ambientali.
+            </p>
+            <iframe 
+              src="https://docs.google.com/forms/d/e/1FAIpQLSeJVviI2jo71QJG2Ah6ULN4eOVoCBtPzcAHODmahp6fHjOygw/viewform?embedded=true" 
+              width="100%" 
+              height="570px" 
+              title="Google Form">
+              Caricamento…
+            </iframe>
+          </motion.div>
+        </Col>
+      </Row>
+      {/* <Row className="justify-content-center mt-5">
         <Col md={10} lg={8}>
           <motion.div
             initial={{ opacity: 0, y: 50 }}
@@ -77,28 +120,8 @@ const Home = () => {
             </Button>
           </motion.div>
         </Col>
-      </Row>
-      <Row className="justify-content-center mt-5">
-        <Col md={10} lg={8}>
-          <motion.div
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1.5 }}
-          >
-            <h2 className="text-secondary mb-3">Unisciti al cambiamento!</h2>
-            <p className="text-muted mb-4">
-              La lotta contro la disinformazione è una battaglia che possiamo vincere solo insieme. Condividi la conoscenza, verifica le notizie e diventa parte attiva della comunità che protegge il nostro pianeta dalle bugie. 
-            </p>
-            <Button
-              variant="success"
-              size="smß"
-              href="/community"
-              className="px-4 py-2 rounded-pill btn-hover">
-              Unisciti a noi
-            </Button>
-          </motion.div>
-        </Col>
-      </Row>
+      </Row> */}
+
     </Container>
   );
 };
